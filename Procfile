43375fca9c9932f6f5ec9d6718a9d93940a59e00
Adrian <a.wolanowski98@gmail.com>
2021-10-21 12:50:11 +0100

    heroku support

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn myproject.wsgi
-web: python manage.py collectstatic
-web: python manage.py runserver 0.0.0.0:$PORT
+release: python manage.py collectstatic
+release: python manage.py runserver 0.0.0.0:$PORT
+release: python manage.py migrate
--- quiz/settings.py
+++ quiz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$1k!x$j0z*a!zqvgapfh8lr1vyy8mb*w_o7%wi$3ycsm-ze*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -75,13 +76,14 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
